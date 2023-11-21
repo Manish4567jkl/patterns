4ab2e6fa902c582f22f9f29117488241bbe2ea72
Manish <114460202+Manish4567jkl@users.noreply.github.com>
2023-11-22 00:10:46 +0530

    Update threads.java

--- threads.java
+++ threads.java
@@ -1,1 +1,26 @@
+class MyThread extends Thread{
+    public void run(){
+        for(int i=1;i<3;i++){
+            System.out.println("from thread 1"+i);
+        }
+        System.out.println("exit fromm  A");
+    }
+}
+
+class MyThread1 extends Thread{
+    {
+        for(int i=1;i<3;i++){
+            System.out.println("from thread 2"+i);
+        }
+        System.out.println("exit fromm  b");
+    }
+}
+public class thr {
+    public static void main(String[] args) {
+        MyThread a = new MyThread();
+        a.start();
+
+        new MyThread1().start();
+    }
+}
 
